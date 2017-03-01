45f74089a761ed710a9807d6d98e446de79a9f02
Andrzej Pawlowicz <andrzej.pawlowicz@3mdeb.com>
2017-03-01 14:51:31 +0100

    spd: cleanup of spd module interface

--- spd.c
+++ spd.c
@@ -15,6 +15,10 @@
 
 #define NULL 0
 
+#define AMD_INDEX_IO_PORT	0xCD6
+#define AMD_DATA_IO_PORT	0xCD7
+#define AMD_SMBUS_BASE_REG	0x2C
+
 #define SMBHSTSTS smbusbase
 #define SMBHSTCNT smbusbase + 2
 #define SMBHSTCMD smbusbase + 3
@@ -22,6 +26,11 @@
 #define SMBHSTDAT smbusbase + 5
 
 extern void wait_keyup();
+
+int get_ddr2_module_size(int rank_density_byte, int rank_num_byte);
+int get_ddr3_module_size(int sdram_capacity, int prim_bus_width, int sdram_width, int ranks);
+char* convert_hex_to_char(unsigned hex_org);
+void sb800_get_smb(void);
 
 int smbdev, smbfun;
 unsigned short smbusbase;
